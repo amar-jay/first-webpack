8d399e94acacf5bc3948da77e29fa905e0e33efe
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-06-12 10:30:31 +0300

    dist/ removed

--- dist/fetchdata.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ObjInterface {
-    data: {
-        metadata: {
-            title: string;
-            description: string;
-        };
-    };
-}
-export default function fetchData(): Promise<ObjInterface["data"] | void>;
